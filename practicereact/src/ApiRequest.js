aedebadc96cd2968f949c8a80c8dbd5750beb4c8
Nusnan .A.M <82087528+mohamednusnan@users.noreply.github.com>
2024-11-20 21:08:48 +0530

    hi4

---
+++ practicereact/src/ApiRequest.js
@@ -0,0 +1,13 @@
+const apiRequest =async( url ='', optionsObj, errMsg = null) => {
+    try {
+        const response = await fetch(url,optionsObj)
+        if (!response.ok) throw Error ("please reload the app")
+        
+    } catch (err) {
+        errMsg = err.Message
+    }finally{
+        return errMsg
+    }
+}
+
+export default apiRequest
--- practicereact/src/App.js
+++ practicereact/src/App.js
@@ -2,37 +2,71 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SearchItem from './SearchItem';
+import apiRequest from './ApiRequest';
 
 function App() {
-
+  const [fetchError,setfetchError] =useState(null)
+  const [isLoading,setIsLoading] = useState(true)
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('todo_list'))
+    []
   );
-
   const [search,setSearch] = useState('')
+  const API_URL ='http://localhost:3500/items';
+  useEffect(() =>{
+      const fetchItems = async () => {
+        try {
+          const response = await fetch(API_URL);
+          if (!response.ok) throw Error(
+            "Data not recieved"
+          ) 
+          const listItems = await response.json();
+          setItems(listItems)
+          setfetchError(null)
+        } catch (err) {
+          setfetchError(err.message)
+        } finally{
+          setIsLoading(false)
+        }
+      }
+      setTimeout(() => {
+        (async() =>await fetchItems())()
+      },2000)
+     
+  },[])
+
+
+
+ useEffect(()=> {
+
+ },[] )
 
   const handleCheck = (id) => {
     const listItems =items.map(
       (item) => item.id ===id ? {...item, checked:!item.checked} : item
     )
     setItems(listItems)
-    localStorage.setItem("todo_list",JSON.stringify(listItems))
-  }
+     }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems)
-    localStorage.setItem("todo_list",JSON.stringify(listItems))
+  
   }
   const [newItem, setnewItem] = useState('');
-  const addItem =(item) => {
+  const addItem = async (item) => {
     const id =items.length? items[items.length -1].id + 1 :1;
     const addNewItem ={id,checked:false, item}
     const listItems =[...items, addNewItem]
     setItems(listItems)
-    localStorage.setItem("todo_list",JSON.stringify(listItems))
+    const postOptions ={
+      method :'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify(addNewItem)
+    }
+    const result = await apiRequest(API_URL,postOptions)
+    if (result) setfetchError(result)
   }
 
   const handleSubmit = (e) => {
@@ -55,11 +89,13 @@ function App() {
             search ={search}
             setSearch={setSearch}
             />
-            <Content
+            {isLoading && <p>Loading...</p>}
+            {fetchError && <p>{`Error: ${fetchError}`}</p>}
+            { !isLoading && !fetchError &&<Content
             items ={items .filter(item => (item.item).toLowerCase().includes(search.toLowerCase()))}
             handleCheck ={handleCheck}
             handleDelete ={handleDelete}
-            />
+            />}
             <Footer
             length ={items.length}
             />
